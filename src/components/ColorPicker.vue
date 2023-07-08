4b7840eb3441a7ac1c246870e3868b10afde2012
schegol <yovladdi@yandex.ru>
2023-07-08 04:54:56 +0400

    Init

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="color-picker">
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <ColorPicker
+
+  ></ColorPicker>
+</template>
+
+<script>
+import ColorPicker from '@/components/ColorPicker.vue'
+
+export default {
+  name: 'AAWallet',
+  components: {
+    ColorPicker,
+  }
+}
+</script>
