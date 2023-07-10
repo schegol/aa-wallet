7ce01ed8cbc7a833310a0ed15acf61ae0012a749
schegol <yovladdi@yandex.ru>
2023-07-10 07:49:37 +0400

    More color picker work

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -12,7 +12,7 @@
                         min="0"
                         max="359"
                         step="1"
-                        v-model="initialColor.h"
+                        v-model="initialHue"
                         @change="changeHue"
                         @input="inputHue"
                     >
@@ -26,7 +26,7 @@
                         min="0"
                         max="100"
                         step="1"
-                        v-model="initialColor.s"
+                        :value="initialSaturation"
                         @change="changeSaturation"
                         @input="inputSaturation"
                     >
@@ -40,7 +40,7 @@
                         min="0"
                         max="100"
                         step="1"
-                        v-model="initialColor.l"
+                        :value="initialLightness"
                         @change="changeLightness"
                         @input="inputLightness"
                     >
@@ -57,14 +57,38 @@ export default {
         
     }),
     computed: {
-        initialColor() {
-            return this.color;
+        // initialColor() {
+        //     return this.color;
+        // },
+        initialHue() {
+            return this.color.h;
         },
+        initialSaturation() {
+            return this.color.s;
+        },
+        initialLightness() {
+            return this.color.l;
+        },
+
+        saturationGradient() {
+            return 'linear-gradient(to right, hsl(' + this.color.h + ', 0%, 50%) 0%, hsl(' + this.color.h + ', 100%, 50%) 100%)';
+        },
+
+        hueThumbColor() {
+            return 'hsl(' + this.color.h + ', 100%, 50%)';
+        },
+        saturationThumbColor() {
+            return 'hsl(' + this.color.h + ', ' + this.color.s + '%, 50%)';
+        },
+        lightnessThumbColor() {
+            return 'hsl(' + this.color.h + ', 100%, ' + this.color.l + '%)';
+        },
+        
         size() {
             return this.initialSize + 'px';
         },
         btnColor() {
-            return 'hsl(' + this.color.h + ',' + this.color.s + '%,' + this.color.l + '%)';
+            return 'hsl(' + this.color.h + ', ' + this.color.s + '%, ' + this.color.l + '%)';
         },
     },
     methods: {
@@ -113,6 +137,7 @@ export default {
     }
 
     .color-picker__btn {
+        position: relative;
         display: block;
         width: 100%;
         max-width: calc(v-bind(size) - 10px);
@@ -124,12 +149,154 @@ export default {
         background-color: v-bind(btnColor);
         border: none;
         border-radius: calc(v-bind(size) / 2 - 5px);
+        z-index: 1;
     }
 
     .color-picker__controls-wrapper {
         position: absolute;
         top: 5px;
         left: 10px;
-        z-index: -1;
+        z-index: 0;
+    }
+
+    .color-picker__control-block {
+        position: relative;
+        width: 220px;
+        height: 12px;
+        box-shadow: 0 4px 4px rgba(0, 0, 0, .25);
+        border: 1px solid #000;
+        border-radius: 6px;
+
+        &:not(:last-child) {
+            margin-bottom: 18px;
+        }
+
+        &--hue {
+            background-image: linear-gradient(
+                to right,
+                hsl(0, 100%, 50%) 0%,
+                hsl(36, 100%, 50%) 10%,
+                hsl(72, 100%, 50%) 20%,
+                hsl(108, 100%, 50%) 30%,
+                hsl(144, 100%, 50%) 40%,
+                hsl(180, 100%, 50%) 50%,
+                hsl(216, 100%, 50%) 60%,
+                hsl(252, 100%, 50%) 70%,
+                hsl(288, 100%, 50%) 80%,
+                hsl(324, 100%, 50%) 90%,
+                hsl(359, 100%, 50%) 99.72%
+            );
+        }
+
+        &--saturation {
+            background-image: v-bind(saturationGradient);
+        }
+
+        &--lightness {
+            background-image: linear-gradient(
+                to right,
+                hsl(0, 100%, 0%) 0%,
+                hsl(0, 100%, 100%) 100%
+            );
+        }
+    }
+
+    .color-picker__control {
+        position: absolute;
+        top: 50%;
+        left: 0;
+        width: 220px;
+        transform: translateY(-50%);
+        -webkit-appearance: none;
+        background-color: transparent;
+
+        &:focus {
+            outline: none;
+        }
+
+        &::-webkit-slider-thumb {
+            -webkit-appearance: none;
+        }
+
+        &::-ms-track {
+            width: 100%;
+            color: transparent;
+            background-color: transparent; 
+            border-color: transparent;
+            cursor: pointer;
+        }
+
+        /***/
+        &::-webkit-slider-thumb {
+            height: 18px;
+            width: 18px;
+            border: none;
+            border-radius: 50%;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, .25);
+            cursor: pointer;
+            -webkit-appearance: none;
+        }
+
+        &::-moz-range-thumb {
+            position: relative;
+            height: 18px;
+            width: 18px;
+            border: none;
+            border-radius: 50%;
+            box-shadow: 0 4px 4px rgba(0, 0, 0, .25);
+            cursor: pointer;
+        }
+
+        &::-ms-thumb {
+            position: relative;
+            height: 18px;
+            width: 18px;
+            border: none;
+            border-radius: 50%;
+            box-shadow: 0 4px 4px rgba(0, 0, 0, .25);
+            cursor: pointer;
+        }
+
+        &--hue {
+            &::-webkit-slider-thumb {
+                background-color: v-bind(hueThumbColor);
+            }
+
+            &::-moz-range-thumb {
+                background-color: v-bind(hueThumbColor);
+            }
+
+            &::-ms-thumb {
+                background-color: v-bind(hueThumbColor);
+            }
+        }
+
+        &--saturation {
+            &::-webkit-slider-thumb {
+                background-color: v-bind(saturationThumbColor);
+            }
+
+            &::-moz-range-thumb {
+                background-color: v-bind(saturationThumbColor);
+            }
+
+            &::-ms-thumb {
+                background-color: v-bind(saturationThumbColor);
+            }
+        }
+
+        &--lightness {
+            &::-webkit-slider-thumb {
+                background-color: v-bind(lightnessThumbColor);
+            }
+
+            &::-moz-range-thumb {
+                background-color: v-bind(lightnessThumbColor);
+            }
+
+            &::-ms-thumb {
+                background-color: v-bind(lightnessThumbColor);
+            }
+        }
     }
 </style>
